ef823e56bc4090f093789b14bebcbaa576166b79
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-20 00:52:22 -0700

    sign_up functionality added

---
+++ app.js
@@ -0,0 +1,6 @@
+function ifUserExists() {
+    if (localStorage.getItem('userAccounts') == null) {
+        window.location.replace('file:///C:/Users/abdulqadir/Desktop/JS%20todo-app/auth/signUp/signUp.html')
+    }
+
+}
---
+++ auth/signUp/signUp.js
@@ -0,0 +1,26 @@
+
+var userAccounts = []
+
+
+function signUp() {
+    var getName = document.getElementById('name').value
+    var getEmail = document.getElementById('email').value
+    var getPassword = document.getElementById('password').value
+
+
+    userAccounts.push({ "name": getName, "email": getEmail, "password": getPassword })
+
+    setItem()
+
+
+    window.location.replace('file:///C:/Users/abdulqadir/Desktop/JS%20todo-app/auth/login/login.html')
+
+
+
+
+
+}
+
+function setItem() {
+    localStorage.setItem('userAccounts', JSON.stringify(userAccounts))
+}
