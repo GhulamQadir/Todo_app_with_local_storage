91f714b476df963cda8aadada7e4dae74a7ee7c4
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-20 11:12:05 -0700

    adding todos

--- home/home.js
+++ home/home.js
@@ -10,16 +10,44 @@ var getInfo = "";
 for (var i = 0; i < userAccounts.length; i++) {
     if (userAccounts[i].email === loggedInUser.email && userAccounts[i].password === loggedInUser.password) {
         console.log(userAccounts[i])
-        console.log(loggedInUser.email)
 
         getInfo = userAccounts[i]
-
 
     }
 }
 
-var a = document.getElementById('userInfo')
+var userInfo = document.getElementById('userInfo')
 var userName = document.createElement('h1')
-var userNameText = document.createTextNode(`Welcome, ${getInfo.name}`)
+var userNameText = document.createTextNode(`Welcome ${getInfo.name}`)
 userName.appendChild(userNameText)
-a.appendChild(userName)
+userInfo.appendChild(userName)
+
+
+var toDosArray = getInfo.todos
+console.log(toDosArray)
+function addTodo() {
+    var todo = document.getElementById('todo_field').value
+
+    toDosArray.push(todo)
+
+
+    getInfo.todos = toDosArray
+    console.log(getInfo)
+
+
+    localStorage.setItem('userAccounts', JSON.stringify(userAccounts))
+
+}
+
+
+// for (var i = 0; i < toDosArray.length; i++) {
+//     var todosList = document.getElementById('todos_div')
+
+//     var list = document.createElement('p')
+//     var listText = document.createTextNode(toDosArray[i])
+//     list.appendChild(listText)
+
+//     todosList.appendChild(list)
+
+// }
+
