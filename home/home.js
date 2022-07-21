34e8953095a62b2347d77559e58518d661c0c03e
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-07-20 23:52:38 -0700

    add todo functionality added

--- home/home.js
+++ home/home.js
@@ -11,12 +11,8 @@ var getInfo = "";
 
 for (var i = 0; i < userAccounts.length; i++) {
     if (userAccounts[i].email === loggedInUser.email && userAccounts[i].password === loggedInUser.password) {
-        // console.log(userAccounts[i])
 
         getInfo = userAccounts[i]
-        getInfo.todos = toDosArray
-        toDosArray = userAccounts[i].todos
-        console.log(toDosArray)
 
     }
 }
@@ -31,20 +27,34 @@ console.log(getInfo)
 
 
 function addTodo() {
-    var todo = document.getElementById('todo_field').value
+    var todo = document.getElementById('todo_field')
 
-    toDosArray.push(todo)
+    if (todo.value === "") {
+        console.log("Please enter some value")
+    }
+    else {
+
+        toDosArray.push(todo.value)
 
 
-    getInfo.todos = toDosArray
-    console.log(getInfo)
+        getInfo.todos = toDosArray
+        console.log(getInfo)
 
 
-    localStorage.setItem('userAccounts', JSON.stringify(userAccounts))
+        localStorage.setItem('userAccounts', JSON.stringify(userAccounts))
 
+        todo.value = ""
+    }
 }
 
-
+function getTodos() {
+    var todos = getInfo.todos
+    toDosArray = todos
+    if (!toDosArray) {
+        toDosArray = []
+    }
+}
+getTodos()
 // for (var i = 0; i < toDosArray.length; i++) {
 //     var todosList = document.getElementById('todos_div')
 
